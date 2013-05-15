cadb32b9234a452348d5fc92d1cb2b496ce60b23
kimhyunkang <kimhyunkang@gmail.com>
2013-05-15 22:14:17 +0900

    test for Repository::head()

--- git2_test.rs
+++ git2_test.rs
@@ -5,3 +5,13 @@ fn test_open() {
     let open:Result<@git2::Repository, git2::GitError> = git2::repository::open(".");
     assert_eq!(open.is_ok(), true);
 }
+
+#[test]
+fn test_head() {
+    let head_result = {
+        let open:Result<@git2::Repository, git2::GitError> = git2::repository::open(".");
+        assert_eq!(open.is_ok(), true);
+        open.get().head()
+    };
+    assert_eq!(head_result.is_ok(), true);
+}
