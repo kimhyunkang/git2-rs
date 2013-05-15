2529597e06eba7b6fa4a2565ac65b115a942e633
kimhyunkang <kimhyunkang@gmail.com>
2013-05-15 13:01:29 +0900

    git_repository_discover

--- repository.rs
+++ repository.rs
@@ -1,7 +1,9 @@
-use core::libc::{c_char, c_void, c_int};
+use core::libc::{c_char, c_void, c_int, size_t};
 use core::path::Path;
 
 use error::*;
+
+static PATH_BUF_SZ: uint = 1024u;
 
 // If we want to know actual storage size of git_repository,
 // we have to know the storage size of pthread_mutex_t
@@ -13,6 +15,9 @@ extern "C" {
     /* from <git2/repository.h> */
     fn git_repository_open(out: **git_repository, path: *c_char) -> c_int;
     fn git_repository_free(repo: *git_repository) -> c_void;
+    fn git_repository_discover(path_out: *mut c_char, path_size: size_t,
+                            start_path: *c_char, across_fs: c_int,
+                            ceiling_dirs: *c_char) -> c_int;
 }
 
 pub struct Repository {
@@ -29,6 +34,28 @@ pub fn open(path: &Path) -> Result<@Repository, GitError>
                 Ok( @Repository { repo: ptr_to_repo } )
             } else {
                 Err( err_last() )
+            }
+        }
+    }
+}
+
+pub fn discover(start_path: &Path, across_fs: bool, ceiling_dirs: &Path) 
+    -> Result<Path, GitError>
+{
+    unsafe {
+        let mut buf = vec::from_elem(PATH_BUF_SZ, 0u8 as c_char);
+        do vec::as_mut_buf(buf) |c_path, sz| {
+            do str::as_c_str(start_path.to_str()) |c_start_path| {
+                do str::as_c_str(ceiling_dirs.to_str()) |c_ceiling_dirs| {
+                    let result = git_repository_discover(c_path, sz as size_t,
+                                            c_start_path, across_fs as c_int, c_ceiling_dirs);
+                    if result == 0 {
+                        let path_str = str::raw::from_buf(c_path as *u8);
+                        Ok(Path(path_str))
+                    } else {
+                        Err(err_last())
+                    }
+                }
             }
         }
     }
