49b15c75d3360c28fb22ae9175b2b18c24c5c03e
kimhyunkang <kimhyunkang@gmail.com>
2013-06-12 05:14:18 +0900

    rename test functions

--- test/git2_test.rs
+++ test/git2_test.rs
@@ -1,14 +1,14 @@
 extern mod git2;
 
 #[test]
-fn test_head() {
+fn repo_head() {
     let repo = git2::repository::open("fixture");
     let head_id = repo.head().resolve();
     assert_eq!(head_id.to_str(), ~"21002f5d3f411fe990e13604273a51cd598a4a51")
 }
 
 #[test]
-fn test_lookup() {
+fn repo_lookup() {
     let repo = git2::repository::open("fixture");
 
     match repo.lookup("refs/heads/master") {
@@ -21,19 +21,19 @@ fn test_lookup() {
 }
 
 #[test]
-fn test_empty() {
+fn repo_empty() {
     let repo = git2::repository::open("fixture");
     assert_eq!(repo.is_empty(), false)
 }
 
 #[test]
-fn test_bare() {
+fn repo_bare() {
     let repo = git2::repository::open("fixture");
     assert_eq!(repo.is_bare(), false)
 }
 
 #[test]
-fn test_oid() {
+fn repo_oid() {
     let oid_str = ~"93d8ad7e3f5a300d2b4e18e9f31247a95e5cc37f";
     let oid = git2::oid::from_str(oid_str).get();
     assert_eq!(oid.to_str(), oid_str)
